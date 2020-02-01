aef8e6a2a8bcf5103c1a39be26abbf85418f4948
GrzegorzZmuda <33518770+GrzegorzZmuda@users.noreply.github.com>
2020-02-01 22:16:57 +0100

    working prototype

--- a.py
+++ a.py
@@ -13,13 +13,15 @@ def col(map,posx,posy,deg):
     tracey=posy
     for i in range(200):
         dist=dist+1
-        tracex=tracex+0.1*np.sin(np.deg2rad(deg))
-        tracey=tracey+0.1*np.sin(np.deg2rad(deg)+90)
+        tracex=tracex+0.01*np.sin(np.deg2rad(deg))
+        tracey=tracey+0.01*np.sin(np.deg2rad(deg)+90)
 
         tmpposx=int(round(tracex))
         tmpposy=int(round(tracey))
         if map[tmpposx][tmpposy]==1:
+            print(tmpposx,tmpposy)
             break
+
     return 200 - dist
 
 
@@ -28,11 +30,11 @@ def newscr(screen,map,posx=4,posy=4,rot=0):
 
 
     for i in range(len(screen)):
-        deg=int(i+1/len(screen))*90+90*rot
+        deg=int((i+1)/len(screen))*90+90*rot
 
         tmp=col(map,posx,posy,deg)
-        tmp2=tmp // 20
-        for j in range(tmp//20):
+        tmp2=tmp // 10
+        for j in range(tmp//10):
             screen[i][20-tmp2+j][2]=tmp
 
 
@@ -69,7 +71,7 @@ for i in range(len(map)):
             map[i][j]=1
         if j==0 or j==9:
             map[i][j]=1
-        if i ==5 and 1<j<9:
+        if i ==5 and 2<j<8:
             map[i][j]=1
 
 pygame.init()
@@ -95,24 +97,25 @@ while Running:
     surf = pygame.surfarray.make_surface(scr)
     screen.blit(surf, (0, 0))
     pygame.display.flip()
-
+    print(x,"",y)
+    print("  cos   ")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and x<10:
+            if event.key == pygame.K_w and x<9 and map[x+1][y]!=1:
                 x=x+1
-            elif event.key == pygame.K_a and y>-1:
-                y=y-1
-            elif event.key == pygame.K_s and x>-1:
+            elif event.key == pygame.K_a and y<9 and map[x][y+1]!=1:
+                y=y+1
+            elif event.key == pygame.K_s and x>0 and map[x-1][y]!=1:
                 x=x-1
-            elif event.key == pygame.K_d and y<10:
+            elif event.key == pygame.K_d and y>0 and map[x][y-1]!=1:
                 y=y-1
-            elif event.key == pygame.K_q and x>-1:
+            elif event.key == pygame.K_q :
                 rot=rotl(rot)
-            elif event.key == pygame.K_e and y<10:
+            elif event.key == pygame.K_e :
                 rot=rotr(rot)
 
 
